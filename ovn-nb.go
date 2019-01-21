037a7b02dca14d4badcebcb6183ad60443b0ed8b
oilbeater <liumengxinfly@gmail.com>
2019-01-21 17:27:58 +0800

    feat: add ovn-nb logical_switch_port model

--- ovn-nb.go
+++ ovn-nb.go
@@ -35,3 +35,19 @@ func (ls *LogicalSwitch) UnmarshalJSON(b []byte) (err error) {
 	}
 	return err
 }
+
+type LogicalSwitchPort struct {
+	UUID             UUID   `json:"_uuid,omitempty" structs:"-"`
+	Name             string `json:"name" structs:"name,omitempty"`
+	DynamicAddresses string `json:"dynamic_addresses" structs:"dynamic_addresses,omitempty"`
+}
+
+func (lsp *LogicalSwitchPort) UnmarshalJSON(b []byte) (err error) {
+	var ols map[string]interface{}
+	if err := json.Unmarshal(b, &ols); err == nil {
+		lsp.UUID.GoUUID = ols["_uuid"].([]interface{})[1].(string)
+		lsp.Name = ols["name"].(string)
+		lsp.DynamicAddresses = ols["dynamic_addresses"].(string)
+	}
+	return err
+}
